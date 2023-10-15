13b82438181baaf9afc7f56104c53ca49166ee9d
karry <karryvrcs@gmail.com>
2023-10-16 03:30:43 +0800

    Composition part2

--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,10 @@
 public class Product {
     private String model;
     private String manufacturer;
+
+    // It's better to build a new class [Dimension] and use the composition because some product (digital product) don't actually
+    // have width, height and depth. The other product that actually have width, height and depth could include the attribute Dimension.
+    // i.e.  private Dimension dimension.
     private int width;
     private int height;
     private int depth;
